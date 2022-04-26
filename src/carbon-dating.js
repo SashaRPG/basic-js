821c5b18a4fd898dad7c6a5978581f67b649bed6
SashaRPG <uasasharpg@gmail.com>
2022-04-26 23:02:09 +0300

    feat: first 2 tasks were completed

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity ) {
+
   // remove line with error and write your code here
+  if (isNaN(sampleActivity) || typeof sampleActivity != 'string' || +sampleActivity <= 0 || +sampleActivity > 15){
+    return false;
+  }else{
+    return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (Math.log(2) / HALF_LIFE_PERIOD));
+  }
 }
 
 module.exports = {
