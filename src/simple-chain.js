9bfddebd77e4cc6d5cad31af6dc90a74e16b7c7e
SashaRPG <uasasharpg@gmail.com>
2022-04-27 21:46:31 +0300

    feat: chains solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chains: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chains.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = '') {
+   this.chains.push(value);
+   return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if(
+      typeof position != 'number' ||
+      position % 1 ||
+      position - 1 < 0 ||
+      position - 1 >= this.chains.length
+    ){
+      this.chains = [];
+      throw new Error(`You can't remove incorrect link!`);
+    }else{
+      this.chains.splice(position-1, 1);
+      return this;
+    }
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chains.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return (
+      this.chains
+        .splice(0, this.chains.length)
+        .reduce((acc, el) => `${acc}${acc ? `~~` : ''}( ${el} )`, '') || `( )`
+    );
   }
 };
 
