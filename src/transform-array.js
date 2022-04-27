8f4eeb5c5717eea5467985c1fd4e53eea5fc1b7e
SashaRPG <uasasharpg@gmail.com>
2022-04-27 21:27:17 +0300

    feat: transform-array solved

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)){
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let resultingArr = [];
+  for (let i = 0; i < arr.length; i++){
+    switch(arr[i]){
+      case '--discard-next':
+        i++;
+        break;
+      case '--discard-prev':
+        if (resultingArr.length - 1 >= 0 && arr[i - 1] == resultingArr[resultingArr.length - 1]){
+          resultingArr.splice(resultingArr.length - 1, 1);
+        }
+        break;
+      case '--double-next':
+        if (i + 1 < arr.length){
+          resultingArr.push(arr[i+1]);
+        }
+        break;
+      case '--double-prev':
+        if (resultingArr.length - 1 >= 0 && arr[i-1] == resultingArr[resultingArr.length-1]){
+          resultingArr.push(resultingArr[resultingArr.length-1]);
+        }
+        break;
+      default:
+        resultingArr.push(arr[i]);
+    }
+  }
+  return resultingArr;
 }
 
 module.exports = {
