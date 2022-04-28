8da4781303cc69ff990082ecc80fc59cb53b87de
SashaRPG <uasasharpg@gmail.com>
2022-04-28 21:10:37 +0300

    feat: delete-digit solved

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,9 +11,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let tempArr = String(n).split('');
+
+  if (n >= 0){
+    if (!(n % 10)){
+      return n / 10;
+    }
+    if (tempArr.length == 2){
+      if (+tempArr[tempArr[0]] > tempArr[1]){
+        return 0
+      }else{
+        return 1;
+      }
+    } 
+
+    for (let i = 0; i < tempArr.length - 1; i++){
+      if (tempArr[i] < tempArr[i+1]){
+        tempArr.splice(i, 1);
+        break;
+      }
+    }
+  }else{
+    if (!(n % 10) && tempArr.length > 3){
+      return n / 10;
+    }else if (tempArr.length == 2){
+      if (+('-' + tempArr[tempArr[0]] > tempArr[1])){
+        return 0;
+      }else{
+        return 1;
+      }
+    }
+    for (let i = 0; i < tempArr.length - 1; i++){
+      if (tempArr[i] > tempArr[i+1]){
+        tempArr.splice(i, 1);
+        break;
+      }
+    }
+
+  }
+  return +tempArr.join('');
 }
 
 module.exports = {
